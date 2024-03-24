64326eff3a2949ce120046efcc639eda7aee1051
Arizona Palmeri <84550659+BoracicNards@users.noreply.github.com>
2024-03-24 06:31:02 -0400

    Add icons and logo to sidebar
    Added icons for each page, alignment off

--- BoardRoom/src/app/user.ts
+++ BoardRoom/src/app/user.ts
@@ -1,3 +1,10 @@
+/*
+Class for User objects that contains all information needed for any given User.
+Information to be displayed on profile.
+*/
+import { Post } from "./post";
+
+
 export class User {
     id: number = 0;
     name: string;
@@ -5,6 +12,7 @@ export class User {
     current_tag: string[];
     bio: string;
     pfp: string;
+    posts: Post[]
 
     constructor(name: string, xp_tag: string[], curr_tag: string[],
         bio: string, pfp: string){
@@ -13,6 +21,7 @@ export class User {
             this.current_tag = curr_tag;
             this.bio = bio;
             this.pfp = pfp;
+            this.posts = [];
     }
 
     getName(){
@@ -33,24 +42,37 @@ export class User {
     getID(){
         return this.id;
     }
+    //Updates bio
     setBio(newBio: string){
         this.bio = newBio;
     }
+    //Changes file path to new profile picture
     setPfpPath(newPath: string){
         this.pfp = newPath;
     }
+    //Appends XP tag to array of experience tags
     addXpTag(tag: string){
         this.xp_tag.push(tag)
     }
+    //Finds index of tag to be removed and then removes single item from array
     removeXpTag(tag: string){
         const index = this.xp_tag.indexOf(tag);
         this.xp_tag.splice(index, 1)
     }
+    //Adds current tag to end of current tag array
     addCurrTag(tag: string){
         this.current_tag.push(tag);
     }
+    //Finds index of current tag to be removed and then removes single element
     removeCurrTag(tag: string){
         const index = this.current_tag.indexOf(tag);
         this.current_tag.splice(index, 1);
+    }
+    getPosts(){
+        return this.posts;
+    }
+    //Adds new post to array of Posts made by a certain user
+    addPost(post: Post){
+        this.posts.push(post);
     }
 }
